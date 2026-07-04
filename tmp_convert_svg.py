011b6a576164e2d459a4d0ed1939ca3f0f32aa13
Sukumarkarmakar78 <2024ac05784@wilp.bits-pilani.ac.in>
2026-07-05 00:19:53 +0530

    Fix tmp_convert_svg.py line length for cairosvg import usage

--- tmp_convert_svg.py
+++ tmp_convert_svg.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 from cairosvg import svg2png
 src_dir = Path('screenshots/ci_cd')
-for name in ['ci_pipeline.svg', 'deployment_rollout.svg', 'prometheus_dashboard.svg']:
+for name in [
+    'ci_pipeline.svg',
+    'deployment_rollout.svg',
+    'prometheus_dashboard.svg',
+]:
     svg_path = src_dir / name
     png_path = src_dir / (svg_path.stem + '.png')
     svg2png(url=str(svg_path), write_to=str(png_path), output_width=900)
